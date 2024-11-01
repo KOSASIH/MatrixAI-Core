ebccf22dc9c8d1183d80b09c1ea96103306b8222
KOSASIH <kosasihg88@gmail.com>
2024-11-01 10:48:03 +0700

    Create __init__.py

---
+++ src/utils/__init__.py
@@ -0,0 +1,14 @@
+# src/utils/__init__.py
+
+from .data_processing import (
+    load_data,
+    clean_data,
+    normalize_data,
+    split_data,
+)
+
+from .visualization import (
+    plot_histogram,
+    plot_scatter,
+    plot_correlation_matrix,
+)
